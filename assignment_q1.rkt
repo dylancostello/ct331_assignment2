b13ca5d73e9b5d504b8805f5b1a6ef3b4d3cf9d6
Dylan Costello <dylancos29@hotmail.com>
2017-10-26 21:55:04 +0100

    Question 1 Completed

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 2 7)
+(cons '2 (cons '4 (cons '6 '())))
+(cons "abcde" (cons 1 (cons (cons 3 (cons 6 (cons 9 '()))) '())))
+(list "abcde" 1 (list 3 6 9))
+(append '("abcde") '(1) '((3 6 9)))
